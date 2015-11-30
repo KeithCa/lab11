b739475254e2de8596c7c0bcbffe3f04cc6a2fb5
KeithCa <c12406518@mydit.ie>
2015-11-30 15:23:25 +0000

    Update message_aws_queue.py

--- message_aws_queue.py
+++ message_aws_queue.py
@@ -1,24 +1,3 @@
-kip to content
-This repository
-
-    Pull requests
-    Issues
-    Gist
-
-    @KeithCa
-
-1
-0
-
-    0
-
-KeithCa/lab11
-
-lab11/delete_aws_queue.py
-8b6e823 7 days ago
-@KeithCa KeithCa Update delete_aws_queue.py
-1 contributor
-27 lines (22 sloc) 786 Bytes
 #
 # Author - Paul Doyle Nov 2015
 #
@@ -44,4 +23,4 @@ first_arg = sys.argv[1]
 second_arg = sys.argv[2]
 
 # Get a list of the queues that exists and then print the list out
-conn.delete_queue(first_arg, second_arg)
+conn.send_message(first_arg, second_arg)
